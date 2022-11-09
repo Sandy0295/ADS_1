751b54ba45befbd110898094c34ea7ce95c88704
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2022-11-09 16:44:51 +0000

    Defining required columns and reading the data set
    In this commit, we are defining our own columns and reading the data from the data set using the packages that we imported.

--- Assignment1_Visualization_spyder.py
+++ Assignment1_Visualization_spyder.py
@@ -11,3 +11,11 @@ import pandas as pd
 #importing the pyplot from matplotlib package to plot the visualization graphs
 import matplotlib.pyplot as plt
 
+#defining the columns that are required for this scenario from the data set
+required_cols = ['elo1_pre','elo2_pre','elo_prob1','elo_prob2','raptor1_pre','raptor2_pre','raptor_prob1','raptor_prob2','score1','score2','total_rating']
+
+#reading the csv file containig our data and reading only first five records with the chosen columns
+game_data = pd.read_csv('C:/Users/sande/MS-DS/ADS-1/Assg-1/nba_elo_latest.csv',usecols=required_cols,nrows=5)
+
+#setting the index value to 1 instead of default value 0
+game_data.index = game_data.index + 1
