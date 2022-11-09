75825daaebbe68e08ef8f7a580345a816939638a
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2022-11-09 16:43:22 +0000

    Importing pandas and matplotlib packages
    In this commit, pandas and pyplot from matplotlib packages are imported that are used for reading the data set and for plotting the graphs respectively

---
+++ Assignment1_Visualization_spyder.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 06:05:32 2022
+
+@author: sande
+"""
+
+#importing pandas package to read the files(data set)
+import pandas as pd
+
+#importing the pyplot from matplotlib package to plot the visualization graphs
+import matplotlib.pyplot as plt
+
